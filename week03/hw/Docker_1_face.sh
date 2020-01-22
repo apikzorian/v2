1790e9c5c2bf51acde099d53f8c3c6ce33b9e9ab
Apik Zorian <apikzorian@gmail.com>
2020-01-22 08:50:16 -0500

    Delete Docker_1_face.sh

--- week03/hw/Docker_1_face.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-!xhost + local:root
-#sudo docker run --user=root --env="DISPLAY" --name face_detect2 --privileged --network face_net -v "$PWD":/hw3_vol -ti az_ub bash
-
-sudo docker run --user=root --env="DISPLAY" --name face_detect3 --privileged --network face_net -v "$PWD":/hw3_vol -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" -ti az_ub bash
